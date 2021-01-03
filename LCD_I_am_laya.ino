b8dd1515953b5f291abb3786849c4669e56154b9
Laya Mathikshara <62396571+layamathikshara@users.noreply.github.com>
2021-01-03 19:51:12 +0530

    Add files via upload

---
+++ LCD_I_am_laya.ino
@@ -0,0 +1,15 @@
+#include <LiquidCrystal.h>
+LiquidCrystal dot_matrix(12,11,5,4,3,2);
+#These are the are the matrix numbers that will glow.
+
+void setup() {
+dot_matrix.begin(16,4);
+#This is where the cursor starts to type the first letter. 
+}
+
+void loop() {
+dot_matrix.print("I am laya") ;
+delay (1000);
+dot_matrix.clear();
+delay (1000);
+}      
